6c6e4a5878c0a49982d12eb97d8cd54f9b7e9fb8
Marlon Izidoro <mizidoro684@gmail.com>
2026-05-10 11:06:12 -0300

    ajustando correção

--- Aula12ex/exercicio014/script.js
+++ Aula12ex/exercicio014/script.js
@@ -2,7 +2,7 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('img')
     var data = new Date()
-    var hora = 13
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         img.src = 'manha.jpg'
