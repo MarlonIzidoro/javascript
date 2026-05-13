22517a7cf78bf76bfd97876fa863ae9628d8b103
Marlon Izidoro <mizidoro684@gmail.com>
2026-05-13 12:11:09 -0300

    Aula13

---
+++ Aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+
+
+do {
+    console.log(`Passo ${c}`)
+} while (c <= 6)
+
+
+
+/*
+while (c <= 3) {
+    console.log(`Passo ${c}`)
+}
+*/
+
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
